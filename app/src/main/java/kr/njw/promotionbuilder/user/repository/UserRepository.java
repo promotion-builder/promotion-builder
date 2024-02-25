9d47522dce74701b9c293747aa8a81a0862ad90e
Na Jongwoo <njw1204@naver.com>
2024-02-26 04:49:14 +0900

    feat: UserRepository에 필요한 메서드 정의

--- app/src/main/java/kr/njw/promotionbuilder/user/repository/UserRepository.java
+++ app/src/main/java/kr/njw/promotionbuilder/user/repository/UserRepository.java
@@ -1,14 +1,22 @@
 package kr.njw.promotionbuilder.user.repository;
 
 
+import jakarta.persistence.LockModeType;
 import kr.njw.promotionbuilder.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findUserByUsernameAndDeletedAtNull(String username);
-    Optional<User> findUserByRefreshTokenAndDeletedAtNull(String refreshToken);
+    Optional<User> findByIdAndDeletedAtNull(Long id);
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameAndDeletedAtNull(String username);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<User> findForUpdateByRefreshTokenAndDeletedAtNull(String refreshToken);
 }
